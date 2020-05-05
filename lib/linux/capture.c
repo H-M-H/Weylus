be08ec7f5866e4c14b7a0c4d7d1d6ba2c6aeb253
HMH <henry@freedesk.net>
2020-05-05 11:48:20 +0200

    Make menus in window captures visible.
    Unfortunately menus do not seem to belong to windows itself, so capture
    the window by capturing a cropped version of the root window. Most menus
    should be visible like that.
    Only do so while the window is active though, otherwise we might capture
    stuff that is not supposed to be captured.

--- lib/linux/capture.c
+++ lib/linux/capture.c
@@ -77,7 +77,8 @@ void destroy_capture(CaptureContext* ctx, Error* err)
 {
 	XShmDetach(ctx->winfo.disp, &ctx->shminfo);
 	XDestroyImage(ctx->ximg);
-	if (shmdt(ctx->shminfo.shmaddr) != 0) {
+	if (shmdt(ctx->shminfo.shmaddr) != 0)
+	{
 		fill_error(err, 1, "Failed to detach shared memory!");
 	}
 	free(ctx);
@@ -85,12 +86,12 @@ void destroy_capture(CaptureContext* ctx, Error* err)
 
 void capture_sceen(CaptureContext* ctx, struct Image* img, Error* err)
 {
-	Window junkroot;
-	int x, y;
+	Window root;
+	int junkx = 0, junky = 0;
 	unsigned int width, height;
 	unsigned int bw, depth;
 	if (!XGetGeometry(
-			ctx->winfo.disp, ctx->winfo.win, &junkroot, &x, &y, &width, &height, &bw, &depth))
+			ctx->winfo.disp, ctx->winfo.win, &root, &junkx, &junky, &width, &height, &bw, &depth))
 	{
 		ERROR(err, 1, "Failed to get window geometry!");
 	}
@@ -106,7 +107,34 @@ void capture_sceen(CaptureContext* ctx, struct Image* img, Error* err)
 			return;
 		}
 	}
-	XShmGetImage(ctx->winfo.disp, ctx->winfo.win, ctx->ximg, 0, 0, 0x00ffffff);
+
+	Window junkwin;
+	int x = 0, y = 0;
+	Window* active_window = 0;
+	unsigned long size;
+
+	active_window = (Window*)get_property(
+		ctx->winfo.disp,
+		DefaultRootWindow(ctx->winfo.disp),
+		XA_WINDOW,
+		"_NET_ACTIVE_WINDOW",
+		&size,
+		err);
+	if (*active_window == ctx->winfo.win)
+	{
+		// capture window within its root so menus are visible as strictly speaking menus do not
+		// belong to the window itself ...
+		XTranslateCoordinates(
+			ctx->winfo.disp, ctx->winfo.win, root, junkx, junky, &x, &y, &junkwin);
+		XShmGetImage(ctx->winfo.disp, root, ctx->ximg, x, y, 0x00ffffff);
+	}
+	else
+	{
+		// ... but only if it is the active window as we might be recording the wrong thing
+		// otherwise. If it is not active just record the window itself.
+		XShmGetImage(ctx->winfo.disp, ctx->winfo.win, ctx->ximg, 0, 0, 0x00ffffff);
+	}
+	free(active_window);
 	img->width = ctx->ximg->width;
 	img->height = ctx->ximg->height;
 	img->data = ctx->ximg->data;
