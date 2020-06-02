1c2d63ea1754995975f4846c5d2ad0e4c9f95e73
HMH <henry@freedesk.net>
2020-06-02 02:36:06 +0200

    Rename libs on windows.

--- deps/build.sh
+++ deps/build.sh
@@ -5,3 +5,10 @@ set -ex
 ./download.sh
 ./x264.sh
 ./ffmpeg.sh
+
+if [ "$RUNNER_OS" == "Windows" ]; then
+    cd deps/dist/lib
+    for l in *.a; do
+        cp "$l" "${${l#lib}%.a}.lib"
+    done
+fi
