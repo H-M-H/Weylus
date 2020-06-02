dcd8a7dd052253869f45809ef9d4f1b92e8c3979
HMH <henry@freedesk.net>
2020-06-03 00:42:01 +0200

    Rename *.a -> *.lib, libx264.lib -> x264.lib on windows.

--- deps/build.sh
+++ deps/build.sh
@@ -5,14 +5,23 @@ set -ex
 ./download.sh
 
 if [ "$RUNNER_OS" == "Windows" ]; then
-	export CC=cl
-	export FFMPEG_EXTRA_ARGS="--toolchain=msvc"
-	export FFMPEG_CFLAGS="-I../dist/include"
-	export FFMPEG_LIBRARY_PATH="-LIBPATH:../dist/lib"
+    export CC=cl
+    export FFMPEG_EXTRA_ARGS="--toolchain=msvc"
+    export FFMPEG_CFLAGS="-I../dist/include"
+    export FFMPEG_LIBRARY_PATH="-LIBPATH:../dist/lib"
 else
-	export FFMPEG_CFLAGS="-I../dist/include"
-	export FFMPEG_LIBRARY_PATH="-L../dist/lib"
+    export FFMPEG_CFLAGS="-I../dist/include"
+    export FFMPEG_LIBRARY_PATH="-L../dist/lib"
 fi
 
 ./x264.sh
 ./ffmpeg.sh
+
+if [ "$RUNNER_OS" == "Windows" ]; then
+    cd dist/lib
+    for l in *.a; do
+        d=${l#lib}
+        cp "$l" "${d%.a}.lib"
+    done
+    cp libx264.lib x264.lib
+fi
