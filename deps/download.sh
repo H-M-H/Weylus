c390c3bce2692f7d43b52edaab28f2d584e9cf31
HMH <henry@freedesk.net>
2021-02-11 00:14:04 +0100

    Update libva to 2.10.0

--- deps/download.sh
+++ deps/download.sh
@@ -7,7 +7,7 @@ git clone -b stable https://code.videolan.org/videolan/x264.git x264
 git clone -b n4.3 https://git.ffmpeg.org/ffmpeg.git ffmpeg
 if [ "$RUNNER_OS" == "Linux" ]; then
     git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
-    git clone -b 2.8.0 https://github.com/intel/libva
+    git clone -b 2.10.0 https://github.com/intel/libva
 fi
 cd ffmpeg
 
