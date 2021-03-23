2ea5d5f02de0e58e7a482646661821a13243dfc9
HMH <henry@freedesk.net>
2021-03-23 01:14:57 +0100

    Add some docs on the Capturable trait.

--- src/capturable/mod.rs
+++ src/capturable/mod.rs
@@ -30,9 +30,18 @@ where
 }
 
 pub trait Capturable: Send + BoxCloneCapturable {
+    /// Name of the Capturable, for example the window title, if it is a window.
     fn name(&self) -> String;
+
+    /// Return x, y, width, height of the Capturable as floats relative to the absolute size of the
+    /// screen. For example x=0.5, y=0.0, width=0.5, height=1.0 means the right half of the screen.
     fn geometry_relative(&self) -> Result<(f64, f64, f64, f64), Box<dyn Error>>;
+
+    /// Callback that is called right before input is simulated.
+    /// Useful to focus the window on input.
     fn before_input(&mut self) -> Result<(), Box<dyn Error>>;
+
+    /// Return a Recorder that can record the current capturable.
     fn recorder(&self, capture_cursor: bool) -> Result<Box<dyn Recorder>, Box<dyn Error>>;
 }
 
