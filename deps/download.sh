ac4864bfbdf2a1356cac361e9e23e4edba49208e
HMH <henry@freedesk.net>
2021-03-31 01:29:31 +0200

    Only clone what's necessary.

--- deps/download.sh
+++ deps/download.sh
@@ -3,11 +3,11 @@
 set -ex
 
 rm -rf x264 ffmpeg
-git clone -b stable https://code.videolan.org/videolan/x264.git x264
-git clone -b n4.3.2 https://git.ffmpeg.org/ffmpeg.git ffmpeg
+git clone --depth 1 -b stable https://code.videolan.org/videolan/x264.git x264
+git clone --depth 1 -b n4.3.2 https://git.ffmpeg.org/ffmpeg.git ffmpeg
 if [ "$RUNNER_OS" == "Linux" ]; then
-    git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
-    git clone -b 2.10.0 https://github.com/intel/libva
+    git clone --depth 1 https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+    git clone --depth 1 -b 2.10.0 https://github.com/intel/libva
 fi
 cd ffmpeg
 
