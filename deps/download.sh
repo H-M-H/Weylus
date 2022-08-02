67b5f30a3b8fbc58680b585c620e42417fe5eb11
HMH <henry@freedesk.net>
2022-08-02 18:02:06 +0200

    Update ffmpeg and libva.

--- deps/download.sh
+++ deps/download.sh
@@ -4,10 +4,10 @@ set -ex
 
 rm -rf x264 ffmpeg
 git clone --depth 1 -b stable https://code.videolan.org/videolan/x264.git x264
-git clone --depth 1 -b n5.0 https://git.ffmpeg.org/ffmpeg.git ffmpeg
+git clone --depth 1 -b n5.1 https://git.ffmpeg.org/ffmpeg.git ffmpeg
 if [ "$TARGET_OS" == "linux" ]; then
     git clone --depth 1 https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
-    git clone --depth 1 -b 2.14.0 https://github.com/intel/libva
+    git clone --depth 1 -b 2.15.0 https://github.com/intel/libva
 fi
 if [ "$TARGET_OS" == "windows" ]; then
     git clone --depth 1 https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
